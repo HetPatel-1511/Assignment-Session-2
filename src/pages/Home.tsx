3b6e655f402403cca6af80f8125874b98a5d7498
patelhet <het.patel@roimaint.com>
2026-01-21 20:05:59 +0530

    debounce search

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,74 +1,19 @@
 import { useContext, useEffect, useState } from 'react'
-// import './App.css'
-import Navbar from '../components/Navbar'
 import ProductListItem from '../components/ProductListItem'
-import { v4 as uuidv4 } from 'uuid';
 import StatsCard from '../components/StatsCard';
-import TextInput from '../components/TextInput';
-import Modal from '../components/Modal';
 import { ThemeContext } from '../provider/ThemeContext';
 import useProducts from '../hooks/useProducts';
 import useProductCategories from '../hooks/useProductCategories';
 import ErrorCard from '../components/ErrorCard';
-import { CartContext } from '../provider/CartContext';
 
 
 function Home() {
-  // const [products, setProducts] = useState<Array<{
-  //   id: string,
-  //   name: string,
-  //   price: number,
-  //   category: string,
-  //   quantity: number
-  // }>>([]);
-
-  // const [displayProducts, setDisplayProducts] = useState<Array<{
-  //   id: string,
-  //   name: string,
-  //   price: number,
-  //   category: string,
-  //   quantity: number
-  // }>>([]);
-
-  // const [newProduct, setNewProduct] = useState<{
-  //   name: string,
-  //   price: number,
-  //   category: string,
-  //   quantity: number
-  // }>({
-  //   name: "",
-  //   price: 0,
-  //   category: "",
-  //   quantity: 0
-  // });
-
-  // const [newProductError, setNewProductError] = useState<{
-  //   name: string,
-  //   price: string,
-  //   category: string,
-  //   quantity: string
-  // }>({
-  //   name: "",
-  //   price: "",
-  //   category: "",
-  //   quantity: ""
-  // });
-
-  // const [showModal, setShowModal] = useState<boolean>(false);
-  const [showEditModal, setShowEditModal] = useState<boolean>(false);
-  const [editProductId, setEditProductId] = useState<string>("");
-
-
-
-  ////////////////////////////////////////////////////////////////////////////////////
-  ////////////////////////////////////////////////////////////////////////////////////
-  ////////////////////////////////////////////////////////////////////////////////////
-
   const { theme } = useContext(ThemeContext)
   const { products, status: productsStatus, error: productsError, fetchProducts } = useProducts()
   const { productsCategory, fetchProductCategories } = useProductCategories()
   const [search, setSearch] = useState<string>("");
   const [filter, setFilter] = useState<string>("All");
+  const [debounceSearch, setDebounceSearch] = useState(search);
 
   useEffect(() => {
     const fetch = async () => {
@@ -79,10 +24,10 @@ function Home() {
 
   useEffect(() => {
     const fetch = async () => {
-      await fetchProducts(search, filter)
+      await fetchProducts(debounceSearch, filter)
     }
     fetch()
-  }, [search, filter])
+  }, [debounceSearch, filter])
 
   const handleSearch = (e: any) => {
     if (e.target.value != "" || e.target.value != null) {
@@ -98,127 +43,16 @@ function Home() {
     setFilter(e.target.value)
   }
 
-  // const handleSubmit = (e: any) => {
-  //   e.preventDefault();
-  //   const name = newProduct.name.trim();
-  //   const price = Number(newProduct.price);
-  //   const category = newProduct.category.trim().toLowerCase();
-  //   const quantity = Number(newProduct.quantity);
-  //   let error = false;
-  //   setNewProductError({
-  //     name: "",
-  //     price: "",
-  //     category: "",
-  //     quantity: ""
-  //   })
-  //   if (name == "") {
-  //     setNewProductError((error) => { return { ...error, name: "Product name is required" } })
-  //     error = true;
-  //   }
-  //   if (products.find((ps) => ((ps.name == name && (showEditModal ? ps.id!=editProductId: true))))) {
-  //     setNewProductError((error) => { return { ...error, name: "Product with this name already exist" } })
-  //     error = true;
-  //   }
-  //   if (price == 0) {
-  //     setNewProductError((error) => { return { ...error, price: "Product price is required" } })
-  //     error = true;
-  //   }
-  //   if (category == "") {
-  //     setNewProductError((error) => { return { ...error, category: "Product category is required" } })
-  //     error = true;
-  //   }
-  //   if (!error) {
-  //     if (showEditModal) {
-  //       setProducts(ps => {
-  //         const newProducts: typeof products =ps.map(product => product.id==editProductId ? {...product, name: name, price: price, category: category, quantity: quantity} : product)
-  //         localStorage.setItem('products', JSON.stringify(newProducts))
-  //         return newProducts
-  //       })
-  //       setNewProduct({ name: "", price: 0, category: "", quantity: 0 })
-  //       setNewProductError({ name: "", price: "", category: "", quantity: "" })
-  //       setShowEditModal(false);
-  //       setEditProductId("");
-  //     } else {
-  //       setProducts((ps) => {
-  //         const newProducts: typeof products = [...ps, { id: uuidv4(), name: name, price: price, category: category, quantity: quantity }]
-  //         localStorage.setItem('products', JSON.stringify(newProducts))
-  //         return newProducts
-  //       })
-  //       setNewProduct({ name: "", price: 0, category: "", quantity: 0 })
-  //       setNewProductError({ name: "", price: "", category: "", quantity: "" })
-  //       setShowModal(false);
-  //     }
-  //   }
-  // }
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebounceSearch(search);
+    }, 500);
 
-  // useEffect(() => {
-  //   const productsList = localStorage.getItem("products")
-  //   if (productsList) {
-  //     setProducts(JSON.parse(productsList));
-  //   }
-  // }, [])
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [search]);
 
-  // useEffect(() => {
-  //   setDisplayProducts([...products])
-  // }, [products])
-
-  // useEffect(() => {
-  //   const searchTerm = search.trim().toLowerCase();
-  //   const filterValue = filter.toLowerCase();
-  //   if ((filterValue != "all" && filterValue != "") || searchTerm!="") {
-  //     setDisplayProducts([...products.filter((p) => p.category.toLowerCase() == filterValue || p.name.toLowerCase().includes(searchTerm) || p.category.toLowerCase().includes(searchTerm))])
-  //   } else {
-  //     setDisplayProducts([...products])
-  //   }
-  // }, [products, search, filter])
-
-  // const handleAddProductClick = () => {
-  //   setShowModal(true)
-  // }
-
-  // const handleCloseModal = () => {
-  //   setShowModal(false);
-  //   setShowEditModal(false);
-  //   setEditProductId("");
-  //   setNewProduct({
-  //     name: "",
-  //     price: 0,
-  //     category: "",
-  //     quantity: 0
-  //   })
-  //   setNewProductError({
-  //     name: "",
-  //     price: "",
-  //     category: "",
-  //     quantity: ""
-  //   })
-  // }
-
-  // const handleDelete = (id:string) => {
-  //   console.log("id");
-  //   console.log(id);
-
-  //   // setProducts((products)=>products.filter((product)=>product.id!=id))
-  //   setProducts((products) => {
-  //       const newProducts: typeof products = products.filter((product)=>product.id!=id)
-  //       localStorage.setItem('products', JSON.stringify(newProducts))
-  //       return newProducts
-  //     })
-  // }
-
-  // const handleEdit = (id:string) => {
-  //   setShowEditModal(true);
-  //   setEditProductId(id);
-  //   const productToEdit = products.find(product=>product.id==id)
-  //   if (productToEdit) {
-  //     setNewProduct({
-  //       name: productToEdit.name,
-  //       price: productToEdit.price,
-  //       category: productToEdit.category,
-  //       quantity: productToEdit.quantity,
-  //     })
-  //   }
-  // }
   return (
     <>
       <div className="w-full max-w-md mx-auto mt-4">
@@ -299,49 +133,6 @@ function Home() {
                     <StatsCard title='Total Inventory value' body={`${products.length > 0 ? products.reduce((acc: number, product: any) => acc + (product.stock * product.price), 0) : 0}`} />
                   </div>
                 </div>
-
-                {/* <div className="max-w-sm mx-auto py-5">
-          <h1 className='text-4xl font-bold'>Product Details</h1>
-          <TextInput
-            name="name"
-            labelText="Name"
-            value={newProduct.name}
-            type="text"
-            error={newProductError.name}
-            onChange={handleValueChange}
-          />
-          <TextInput
-            name="price"
-            labelText="Price"
-            value={newProduct.price}
-            type="number"
-            error={newProductError.price}
-            onChange={handleValueChange}
-          />
-          <TextInput
-            name="category"
-            labelText="Category"
-            value={newProduct.category}
-            type="text"
-            error={newProductError.category}
-            onChange={handleValueChange}
-          />
-          <TextInput
-            name="quantity"
-            labelText="Quantity"
-            value={newProduct.quantity}
-            type="text"
-            error={newProductError.quantity}
-            onChange={handleValueChange}
-          />
-          <button
-            type="submit"
-            className="text-white bg-gray-800 box-border border border-transparent hover:bg-brand-strong shadow-xs font-medium leading-5 rounded-base text-sm px-4 py-2.5 focus:outline-none cursor-pointer rounded"
-            onClick={handleSubmit}
-          >
-            Add Product
-          </button>
-        </div> */}
               </>
       }
     </>
